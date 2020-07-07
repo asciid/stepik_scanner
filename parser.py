5b7ff06bfd2f713fe8362a0af47c02d180da5273
[null] <48494379+asciid@users.noreply.github.com>
2020-07-07 11:47:59 +0300

    New version

--- parser.py
+++ parser.py
@@ -2,46 +2,38 @@
 
 from bs4 import BeautifulSoup
 from math import ceil, floor
-import termcolor
 import requests
 import sys
 import os
 
-
 def get_position():
 
-	"""
-	Taking last scanning position from file.
-	Now with some errors handling!
-	"""
-
 	if os.path.exists(posn_file):
-		f = open(posn_file, 'r')
-		content = f.readlines()
-		if len(content) == 0:
-			if os.name != 'nt':
-				err = termcolor.colored('Поздравляю! Вы затёрли файл с позицией.\nСканируйте заново. Так держать!', 'red')
+		with open(posn_file, 'r') as f:
+			content = f.readlines()
+
+			if len(content) == 0 or content[0] in ('\n', '', ' ',):
+
+				answer = input('Your position file has been damaned.\nWould you like to state a scanning point by yourself (y/n): ')
+				
+				if answer.lower() == 'y': 
+					position = int(input('Enter the number of a course: '))
+				else:
+					print('Scan will be started from the beginning.')
+					os.remove(scan_file + '.txt')
+					
+					if os.path.exists(scan_file + '.md'):
+						os.remove(scan_file + '.md')
+
+					position = 1
 			else:
-				err = 'Поздравляю! Вы затёрли файл с позицией.\nСканируйте заново. Так держать!'
-			os.remove(posn_file)
-			os.remove(scan_file)
-			sys.exit(err)
-		elif content[0] in ('\n', '', ' ',):
-			os.remove(scan_file)
-			position = 1
-		else:
-			position = int(content[-1][:-1])
-		f.close()
+				position = int(content[-1][:-1])
 	else:
 		position = 1
 
 	return position	
 
 def get_status(response):
-
-	"""
-	Checking out whether it's 404, 403 or available course.
-	"""
 
 	soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -57,10 +49,6 @@ def get_status(response):
 
 def get_title(response):
 
-	"""
-	Getting a title from html's <title> tag.
-	"""
-
 	soup = BeautifulSoup(response.text, 'html.parser')
 
 	if soup.title.text[3:-7] == 'Stepik':
@@ -68,14 +56,34 @@ def get_title(response):
 	else:
 		return soup.title.text[3:-10]
 
+
+# Trick to disable colored output on Windows
+if os.name != 'nt':
+	from termcolor import colored
+else: 
+	colored = lambda str, color: str
+
+# Arguments
+
+
+md_output = False
+
+if len(sys.argv) > 1:
+	if sys.argv[1] == '--markdown' or sys.argv[1] == '-m':
+		md_output = True
+	elif sys.argv[1] == '--help' or sys.argv[1] == '-h':
+		msg = """stepik_scanner -- Parser for stepik.org\n
+--markdown | -m -- Write in Markdown.
+--help     | -h -- Print this message and exit.
+
+ISSUES: <github.com/asciid/stepik_scanner>"""
+		sys.exit(msg)
+
 URL = "https://stepik.org/course/"
-scan_file = "stepik_courses.txt"
+scan_file = "stepik_courses"
 posn_file = ".position"  
 position = get_position()
-file = open(scan_file, 'a')
-posn = open(posn_file, 'w')
-system = os.name
-banner = """      _             _ _ 
+banner = r"""      _             _ _ 
   ___| |_ ___ _ __ (_) | __     _ __   __ _ _ __ ___  ___ _ __ _ __  _   _ 
  / __| __/ _ \ '_ \| | |/ /    | '_ \ / _` | '__/ __|/ _ \ '__| '_ \| | | |
  \__ \ ||  __/ |_) | |   <     | |_) | (_| | |  \__ \  __/ | _| |_) | |_| |
@@ -83,7 +91,13 @@ banner = """      _             _ _
              |_|         |_____|_|                            |_|    |___/
 """
 
-if position == 1: print(banner)
+print(banner)
+
+if md_output:
+	if not os.path.exists(scan_file + '.md'):
+		with open(scan_file + '.md', 'w') as file:
+			if position == 1: file.write('# Stepik courses\n---\n')
+			file.close()	
 
 while True:
 	try:
@@ -95,30 +109,28 @@ while True:
 
 		if s == 200:
 			title = get_title(r)
-
-			if system == 'nt': status = 'GET'
-			else: status = termcolor.colored('GET', 'green')
+			status = colored('GET', 'green')
 
 			# This code prints course's title in the center. Not a shitcode actually.
 			spaces = (terminal.columns - len(link) - len(title) - 5) / 2
 			print(link, floor(spaces) * ' ', title, ceil(spaces) * ' ', '[', status, ']', sep='')
 
-			file.write(link + ' ' + title + ' ' + '\n')
-		else:
-			if system == 'nt': status = s
-			else: status = termcolor.colored(s, 'red')
+			if md_output:
+				with open(scan_file + '.md', 'a') as file:
+					file.write('1. [{}]({})\n\n'.format(title, link))
+			else:
+				with open(scan_file + '.txt', 'a') as file:
+					file.write(link + ' ' + title + ' ' + '\n')
+		else:	
+			status = colored(s, 'red')
 			print(link.ljust(terminal.columns - 5), '[', status, ']', sep='')
 
 		position += 1
 
-	except KeyboardInterrupt:
+		with open(posn_file, 'w') as posn:
+			posn.write('%d\n' % position)
 
-		if system == 'nt': exit = '\n[ Завершение работы ]'
-		else: exit = termcolor.colored('\n[ Завершение работы ]', 'red') 
+	except :
 
-		posn.write('%d\n' % position)
-
-		posn.close()
-		file.close()
-
+		exit = colored('\n[ Завершение работы ]', 'red') 
 		sys.exit(exit)
